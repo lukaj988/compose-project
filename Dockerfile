86eec62b7aa64392724d01e613995d9a42fcf606
Luka Jovanovic <lukaj988@gmail.com>
2026-09-07 07:45:06 +0000

    Dodata compose validacija

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx
+
+COPY website/index.html /usr/share/nginx/html/index.html
---
+++ scripts/check-compose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "==== Compose Status ===="
+docker compose ps
+
+echo ""
+echo "==== Containers ===="
+docker ps
